bf39e86c2241090bedf0acee2992eddf47dbe7a8
Zach-Zhang <zachjustdoit@outlook.com>
2020-07-26 10:49:13 +0800

    变态跳台阶的问题

--- src/main/java/com/zach/frequency/JumpStep.java
+++ src/main/java/com/zach/frequency/JumpStep.java
@@ -1,5 +1,7 @@
 package com.zach.frequency;
 
+import java.util.Arrays;
+
 /**
  * @Classname JumpStep
  * @Description: 一只青蛙一次可以跳上 1 级台阶，也可以跳上 2 级。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
@@ -22,6 +24,7 @@ public class JumpStep {
     public static void main(String[] args) {
         System.out.println(3);
     }
+
 
     public int jumpStep(int n) {
         if (n <= 2) {
