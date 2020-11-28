17174a97a58a46d83753dc562bb6e2883944f515
Zach-Zhang <zachjustdoit@outlook.com>
2020-11-28 16:09:26 +0800

    合并有序链表

---
+++ src/main/java/com/zach/frequency/MergeList.java
@@ -0,0 +1,78 @@
+package com.zach.frequency;
+
+/**
+ * Created by Zach
+ * Date :2020/11/28 14:53
+ * Description : 合并两个有序链表;
+ * Version :1.0
+ */
+public class MergeList {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * @return com.zach.frequency.MergeList.ListNode
+     * @Description: 递归处理
+     * @Date 15:55 2020/11/28
+     * @Param [h1, h2]
+     **/
+    public ListNode recursiveMergeList(ListNode h1, ListNode h2) {
+        if (h1 == null) {
+            return h2;
+        }
+        if (h2 == null) {
+            return h1;
+        }
+        if (h1.data <= h2.data) {
+            h1.next = recursiveMergeList(h1.next, h2);
+            return h1;
+        } else {
+            h2.next = recursiveMergeList(h1, h2.next);
+            return h2;
+        }
+
+    }
+
+    /**
+     * @return com.zach.frequency.MergeList.ListNode
+     * @Description 利用哨兵模式处理
+     * @Date 16:04 2020/11/28
+     * @Param [h1, h2]
+     **/
+    public ListNode mergeList(ListNode h1, ListNode h2) {
+        ListNode head = new ListNode(-1);
+        ListNode cur = head;
+        while (h1 != null && h2 != null) {
+            if (h1.data <= h2.data) {
+                cur.next = h1;
+                h1 = h1.next;
+            } else {
+                cur.next = h2;
+                h2 = h2.next;
+            }
+            cur = cur.next;
+        }
+        if (h1 != null) {
+            cur.next = h1;
+        }
+        if (h2 != null) {
+            cur.next = h2;
+        }
+        return head.next;
+    }
+
+    private class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data, ListNode next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+}
