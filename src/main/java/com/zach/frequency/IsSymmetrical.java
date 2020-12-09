374e06865907b7f534a18f18d566f5aab793b6c4
Zach-Zhang <zachjustdoit@outlook.com>
2020-12-09 12:09:42 +0800

    对称的二叉树

---
+++ src/main/java/com/zach/frequency/IsSymmetrical.java
@@ -0,0 +1,111 @@
+package com.zach.frequency;
+
+/**
+ * Created by Zach
+ * Date :2020/12/9 11:59
+ * Description :对称的二叉树
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * <p>
+ * 例如，二叉树[1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2  2
+ * / \ / \
+ * 3 4 4 3
+ * 但是下面这个[1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2  2
+ * \  \
+ * 3  3
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ * Version :1.0
+ */
+public class IsSymmetrical {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isSymmetrical(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetrical(root.left, root.right);
+
+    }
+
+    private static boolean isSymmetrical(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        //递归判断
+        return isSymmetrical(left.left, right.right) && isSymmetrical(left.right, right.left);
+    }
+
+    static class TreeNode {
+        private int val;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+}
