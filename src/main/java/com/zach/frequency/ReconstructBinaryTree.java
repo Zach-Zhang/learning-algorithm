7e416e1e1ea1c8d388c5e304e58afa1c19a4104b
Zach-Zhang <zachjustdoit@outlook.com>
2021-05-11 23:42:46 +0800

    数字序列中的某一位数字

--- src/main/java/com/zach/frequency/ReconstructBinaryTree.java
+++ src/main/java/com/zach/frequency/ReconstructBinaryTree.java
@@ -28,15 +28,31 @@ public class ReconstructBinaryTree {
         return reconstructBinaryTree(pre, 0, pre.length - 1, 0);
     }
 
+    /**
+     * 分治算法,递推子树
+     *
+     * @param pre  先序遍历的数组
+     * @param preL 先序遍历的根节点索引
+     * @param preR 递归树的右边界，即数组右边界
+     * @param inL  递归树的左边界,即数组的左边界
+     * @return
+     */
     private static TreeNode reconstructBinaryTree(int[] pre, int preL, int preR, int inL) {
+        //preL大于preR表明已经越过叶子节点,相等则表示就是自己
         if (preL > preR) {
             return null;
         }
+        //先序遍历中的根节点
         TreeNode root = new TreeNode(pre[preL]);
+        //找到根节点在中序遍历中的索引值
         Integer inIndex = indexForOrders.get(root.val);
+        //
         int leftTreeSize = inIndex - inL;
+        //左子树的根节点就是 左子树的(前序遍历）第一个，就是+1,左边边界就是left，右边边界是中间区分的idx-1
         root.left = reconstructBinaryTree(pre, preL + 1, preL + leftTreeSize, inL);
-        root.right = reconstructBinaryTree(pre, preL + leftTreeSize + 1, preR, inL + leftTreeSize + 1);
+        //由根节点在中序遍历的inIndex 区分成2段,inIndex 就是根
+        //右子树的根，就是右子树（前序遍历）的第一个,就是当前根节点 加上左子树的数量,左边界是=根节点+1,右边界是先序遍历的右边界索引,
+        root.right = reconstructBinaryTree(pre, preL + leftTreeSize + 1, preR, inIndex + 1);
         return root;
     }
 
