a93cef8ee70e76c452763ce0faad66a903d61832
Zach-Zhang <zachjustdoit@outlook.com>
2021-05-25 23:05:34 +0800

    把数字翻译成字符串

--- src/main/java/com/zach/frequency/TranslateNum.java
+++ src/main/java/com/zach/frequency/TranslateNum.java
@@ -11,9 +11,17 @@ package com.zach.frequency;
 public class TranslateNum {
     public static void main(String[] args) {
         int count = translateNum(12258);
+        int i = dfsTranslateNum(12258);
         System.out.println(count);
+        System.out.println(i);
     }
 
+    /**
+     * 动态规划
+     *
+     * @param num
+     * @return
+     */
     public static int translateNum(int num) {
         String src = String.valueOf(num);
         int p = 0, q = 0, r = 1;
@@ -30,5 +38,30 @@ public class TranslateNum {
             }
         }
         return r;
+    }
+
+    /**
+     * dfs递归
+     */
+    public static int dfsTranslateNum(int num) {
+        String str = String.valueOf(num);
+        return dfs(str, 0);
+    }
+
+
+    public static int dfs(String str, int index) {
+        //如果当前的下标大于等于字符串的长度-1,则说明当前位置是由上一次跳到此处的,则直接返回1
+        if (index >= str.length() - 1) {
+            return 1;
+        }
+        //先求出每一次都翻译一个字符的方法数
+        int res = dfs(str, index + 1);
+        //截取两位,查看数字是否在10-26之间
+        String temp = str.substring(index, index + 2);
+        if (temp.compareTo("10") >= 0 && temp.compareTo("26") <= 0) {
+            //从两字符后面开始翻译
+            res += dfs(str, index + 2);
+        }
+        return res;
     }
 }
