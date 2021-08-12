ed126faf26bfdeab069f375f7642f5396ca3b9b4
Zach-Zhang <zachjustdoit@outlook.com>
2021-08-12 08:22:33 +0800

    二叉树的深度

---
+++ src/main/java/com/zach/common/TreeNode.java
@@ -0,0 +1,57 @@
+package com.zach.common;
+
+import java.io.Serializable;
+
+/**
+ * @Description:
+ * @Author Zach
+ * @Date 2021/8/12 7:58
+ * Version :1.0
+ */
+public class TreeNode implements Serializable {
+    private TreeNode left;
+    private TreeNode right;
+    private int val;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(TreeNode left, TreeNode right, int val) {
+        this.left = left;
+        this.right = right;
+        this.val = val;
+    }
+
+    public TreeNode(TreeNode left, TreeNode right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode() {
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
---
+++ src/main/java/com/zach/frequency/MaxDepth.java
@@ -0,0 +1,56 @@
+package com.zach.frequency;
+
+import com.zach.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Description:二叉树的深度
+ * @Author Zach
+ * @Date 2021/8/12 7:54
+ * Version :1.0
+ */
+public class MaxDepth {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 后续遍历,此树的深度和其左（右）子树的深度之间的关系。显然，此树的深度 等于 左子树的深度 与 右子树的深度 中的 最大值 +1。
+     *
+     * @param root
+     * @return
+     */
+    public static int maxDepthDFS(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepthDFS(root.getLeft()), maxDepthDFS(root.getRight())) + 1;
+    }
+
+    /**
+     * 使用队列实现层序遍历,
+     * @param root
+     * @return
+     */
+    public static int maxDepthBFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            res++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode.getLeft() != null) {
+                    queue.add(treeNode.getLeft());
+                }
+                if (treeNode.getRight() != null) {
+                    queue.add(treeNode.getRight());
+                }
+            }
+        }
+        return res;
+    }
+}
