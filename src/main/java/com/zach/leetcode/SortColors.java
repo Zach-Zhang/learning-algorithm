453eae14b9ec6f04213195bb568970910200f1d5
Zach-Zhang <zach_ing@163.com>
2023-08-13 16:07:54 +0800

    修改测试

--- src/main/java/com/zach/leetcode/SortColors.java
+++ src/main/java/com/zach/leetcode/SortColors.java
@@ -21,12 +21,12 @@ package com.zach.leetcode;
  */
 public class SortColors {
     public static void main(String[] args) {
-        int[] nums = new int[]{2,0,2,1,1,0};
-        sortColors3(nums);
-        for (int num : nums) {
+
+        int[] array = new int[]{2,0,2,1,1,0};
+        sortColors3(array);
+        for (int num : array) {
             System.out.println(num);
         }
-
     }
 
     /**
